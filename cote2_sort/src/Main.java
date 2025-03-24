5f4f09f947ad5ac4dc2813d55d462569c6916d67
hyeminililo <ng040115@gmail.com>
2025-03-24 21:18:09 +0900

    [2주차 과제 - 가장 큰 수] feat : 배열 String 변환

--- cote2_sort/src/Main.java
+++ cote2_sort/src/Main.java
@@ -2,29 +2,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int citationsArray[] = {3, 0, 6, 1, 5};
-        System.out.println("result : " + solution(citationsArray));
+            int numbers[] = {3, 30, 34, 5, 9};
+            solution(numbers);
     }
 
-    public static int solution(int[] citations) {
-        Arrays.sort(citations); // {0,1,3,5,6}
-
-        int length = citations.length;
-        int answer = 0;
-
-        // i = h
-        for (int i = 0; i <= length; i++) {
-            // 인용된 횟수가 i보다 많을때마다 체크할 수 있는 변수 선언
-            int count = 0;
-
-            for (int c : citations) {
-                if (c >= i) count++; // 인용횟수가 h보다 많으면 count + 1
-            }
-            // 현재 count 값보다 i가 크다면 -> // 1번 조건 으로 따지면 2번 조건이 바로 나오게 됨 answer 갱신
-            if (count >= i) {
-                answer = i;
-            }
+    public  static String solution(int[] numbers) {
+        // String 배열로 바꿔주기
+        String[] numbersString = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            numbersString[i] = String.valueOf(numbers[i]);
         }
+        System.out.println(Arrays.toString(numbersString));
+        String answer = "";
         return answer;
     }
 }
