50b15d297f343e5459f8ae0bc9489d3957ad2ed2
hyeminililo <ng040115@gmail.com>
2025-03-24 18:48:25 +0900

    Feat : Array 정렬

--- cote2_sort/src/Main.java
+++ cote2_sort/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+        int citationsArray[] = {3, 0, 6, 1, 5};
+        solution(citationsArray);
+    }
+
+    public static int solution(int[] citations) {
+        Arrays.sort(citations);
+        System.out.println("citations : "+Arrays.toString(citations));
+
+        int answer = 0;
+        return answer;
     }
 }
