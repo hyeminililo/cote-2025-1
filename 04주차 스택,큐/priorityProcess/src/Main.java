50726bd52016e384828943738ffedbf728d25211
hyeminililo <ng040115@gmail.com>
2025-03-30 21:57:00 +0900

    04주차 프록세스 - 큐 순회하면서 priority에 우선순위가 더 높은게 있으면 break

--- 04주차 스택,큐/priorityProcess/src/Main.java
+++ 04주차 스택,큐/priorityProcess/src/Main.java
@@ -12,16 +12,26 @@ public class Main {
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-
+        int matchIndex = 0 ;
+        boolean isPriority = false;
         // 큐 생성
         ArrayDeque<int[]> queue = new ArrayDeque<>();
 
         for(int i=0; i<priorities.length; i++){
-            queue.add(new int[]{priorities[i], i });
+            queue.add(new int[]{priorities[i], i }); //queue([2,0],[1,1],[3,2],[2,3])
         }
-        for (int[] process : queue) {
-            System.out.println("priority: " + process[0] + ", index: " + process[1]);
+        while(!queue.isEmpty()){
+            int[] currentElement = queue.poll();
+
+            for(int[] p : queue){
+                if(p[0] > currentElement[0] ){
+                    isPriority = true;
+                    break;
+                }
+            }
+
         }
+        System.out.println(isPriority);
         return answer;
     }
 }
