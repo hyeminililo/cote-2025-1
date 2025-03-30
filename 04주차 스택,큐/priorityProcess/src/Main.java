05f627a0c4a4be6ed47719db73fe5b1ee1232a74
hyeminililo <ng040115@gmail.com>
2025-03-30 19:34:48 +0900

    04주차 프록세스 - 프로젝트 설정

---
+++ 04주차 스택,큐/priorityProcess/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] priorities = {2, 1, 3, 2};
+        int location = 2;
+        solution.solution(priorities, location);
+    }
+}
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        return answer;
+    }
+}
