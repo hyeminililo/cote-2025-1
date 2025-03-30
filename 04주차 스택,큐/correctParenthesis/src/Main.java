df0846730e9068e4abb2e9c02cc244229aeb7e33
hyeminililo <ng040115@gmail.com>
2025-03-30 22:53:02 +0900

    04주차 올바른 괄호 - 제출 및 실행 통과

--- 04주차 스택,큐/correctParenthesis/src/Main.java
+++ 04주차 스택,큐/correctParenthesis/src/Main.java
@@ -16,11 +16,13 @@ class Solution {
             answer = false;
             return answer;
         }
+        // char 형으로 만든 stack에 s 괄호들 저장
         for (char c : s.toCharArray()) {
+            // 현재 c가  '('면 괄호 시작이므로 넣기
             if (c == '(') {
                 stack.push(c);
             }
-
+            // 현재 c가 ')'면서 stack이 비어있지 않으면 stack의 top 값을 조회하기 -> top이 '('면 둘 다 빼기
             if (c == ')' && !stack.isEmpty()) {
                 if (stack.peek() == '(') {
                     stack.push(c);
@@ -30,7 +32,7 @@ class Solution {
             }
 
         }
-
+        // 괄호가 다 빠져나가면 비어있으므로 answer = true, 그렇지 않으면 false로 변환
         if (stack.isEmpty()) {
             answer = true;
         } else {
